db657aff8020ce9c99ebbb1bab8759fef60f0183
Iain Macleod <macleod@live.co.uk>
2024-10-20 19:26:34 +0100

    Updated player class to assign colours

--- lib/player.rb
+++ lib/player.rb
@@ -11,6 +11,8 @@ attr_accessor :player_name, :player_token
     #if number of player is > 0 (i.e this is the second player being created) then set token to O
     @player_token = "O" if @@NUMBER_OF_PLAYERS > 0
     #update number of player objects created
+    @player_color = :blue
+    @player_color = :red if @@NUMBER_OF_PLAYERS > 0
     @@NUMBER_OF_PLAYERS +=1
   end
 end
